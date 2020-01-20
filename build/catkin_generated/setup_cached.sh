8d4fdcf386f5d2f3e7dba14e93b2d32ae3e5f90c
Unknown <chuanyuyang_charles@foxmail.com>
2020-01-20 15:19:00 +0000

    commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/chuanyu/ROS_workshop/realsense_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/home/chuanyu/ROS_workshop/realsense_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib"
+export LUA_PATH=";;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua"
+export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/chuanyu/anaconda2/bin:/home/chuanyu/bin:/home/chuanyu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/chuanyu/ROS_workshop/realsense_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/chuanyu/ROS_workshop/realsense_ws/build"
+export PYTHONPATH="/home/chuanyu/ROS_workshop/realsense_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/chuanyu/ROS_workshop/realsense_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/chuanyu/ROS_workshop/realsense_ws/src:$ROS_PACKAGE_PATH"
